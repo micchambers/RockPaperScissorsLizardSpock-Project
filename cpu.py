1aebb2d73a63098cbba75926847acb5f18190281
micchambers <micolc76@gmail.com>
2023-03-03 03:05:51 -0500

    defined basic functions of player parent class

---
+++ cpu.py
@@ -0,0 +1,7 @@
+class CPU:
+    def __init__(self):
+        pass
+
+    def random_item_picker(list):
+        random_item = random.choice(list)
+        return random_item
---
+++ game.py
@@ -0,0 +1,9 @@
+class Game:
+    def __init__ (self, playercount):
+        pass
+
+    def game_start():
+        pass
+
+    def game_state():
+        pass
