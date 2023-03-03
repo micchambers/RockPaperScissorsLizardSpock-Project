2b53910e62f7aa69f0aca39fbd872de85bb7c56e
micchambers <micolc76@gmail.com>
2023-03-03 03:23:46 -0500

    condensed player child classes into one file for now

--- player.py
+++ player.py
@@ -4,3 +4,15 @@ class Player:
 
     def set_move(self, move):
         self.move = move
+
+class CPU:
+    def __init__(self):
+        pass
+
+    def random_item_picker(list):
+        random_item = random.choice(list)
+        return random_item
+    
+class Human:
+    def __init__(self) -> None:
+        pass
